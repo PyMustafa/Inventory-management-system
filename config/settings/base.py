d3629b7dfcbe6fb23dee8638b665024f20b7cf4c
Mohgar <mohamedhgar007@gmail.com>
2025-03-20 13:01:38 +0200

    feat(login/logout): handlling the login and logout in the login page and add log out to side bar

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = "/products/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
