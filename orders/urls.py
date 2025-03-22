93f0dd59fe3c99457e307f220a6c6e12cc4e3a3a
Mohgar <mohamedhgar007@gmail.com>
2025-03-22 23:18:18 +0200

    feat(urls,views): create list view for order and order item and enhance the functionality of serching and pagination

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.add_order, name="add-order"),
+    path("order/", views.OrderListView.as_view(), name="order-list"),
+    path("order/<int:pk>/", views.OrderDetailView.as_view(), name="order-detail"),
+    path("orderitem/", views.OrderItemListView.as_view(), name="orderitem-list"),
+
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order, OrderItem
+
+
+class OrderListView(LoginRequiredMixin, ListView):
+    model = Order
+    template_name = "orders/order_list.html"
+    context_object_name = "orders"
+    paginate_by = 10
+
+    def get_queryset(self):
+
+        try:
+            query_set = super().get_queryset()
+            search_query = self.request.GET.get('search')
+
+            if search_query:
+                query_set = query_set.filter(
+                    Q(name__icontains=search_query) | Q(description__icontains=search_query))
+
+            return query_set
+        except Exception as e:
+            print(f"Error filtering products: {e}")
+            return Order.objects.none()
+
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'orders/order_detail.html'
+    context_object_name = 'order'
+
+
+class OrderItemListView(ListView):
+    model = OrderItem
+    template_name = 'orders/orderitem_list.html'
+    context_object_name = 'orderitems'
+    paginate_by = 10
+
+    def get_queryset(self):
+
+        try:
+            query_set = super().get_queryset()
+            search_query = self.request.GET.get('search')
+
+            if search_query:
+                query_set = query_set.filter(
+                    Q(name__icontains=search_query) | Q(description__icontains=search_query))
+
+            return query_set
+        except Exception as e:
+            print(f"Error filtering products: {e}")
+            return OrderItem.objects.none()
+
+
+class OrderItemDetailView(DetailView):
+    model = OrderItem
+    template_name = 'orders/orderitem_detail.html'
+    context_object_name = 'orderitem'
 
 # Create your views here.
